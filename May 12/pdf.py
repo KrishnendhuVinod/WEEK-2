fbf33a32a8467ac261521ee52b9e29fb32a546c8
krishnendhu <krishnendhuvinodkumar393@gmail.com>
2025-05-12 13:27:16 +0530

    Updated pdf.py file

--- May 12/pdf.py
+++ May 12/pdf.py
@@ -1,14 +1,15 @@
-
+#import library
 import fitz
 
+#Define function for extracting
 def extract():
     pdf = fitz.open("llm.pdf")
-    text=[]
-
+    text=[] #initialize empty list to store text
+    #Loop through all pages
     for pages in pdf:
-        texts = pages.get_text()
-        text.append(texts)
-    pdf.close()
-    all="".join(text)
+        texts = pages.get_text() #Extract text
+        text.append(texts) #Appent it
+    pdf.close() #Close file
+    all="".join(text) #Combine all page text to single string
     print( "Extracted text\n ",all)
 extract()
